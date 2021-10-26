7851d31a691acdd6a9d6bc99263417f94c12448d
Topo <topo@asustin.net>
2021-10-26 23:49:10 +1300

    Rotation behaviour

---
+++ Rotating Cube/Assets/Rotation.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public Transform transform;
+    public float degreesPerTick = 0;
+    public float incrementPerTick = 0.2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float degrees = degreesPerTick * Time.deltaTime;
+        transform.Rotate(0, degrees, 0, Space.World);
+        degreesPerTick += incrementPerTick;
+    }
+}
